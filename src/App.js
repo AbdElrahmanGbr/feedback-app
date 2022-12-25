da4f26a4ea709fd1df4a9fc2fe8a84358f104b3c
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-12-25 16:08:42 +0200

    Events and Prop Drilling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import FeedbackList from "./components/FeedbackList";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+    const deleteFeedback = (id) =>{
+        if(window.confirm('Are you Sure you want to DELETE?')){
+            setFeedback(feedback.filter((item)=> item.id !== id))
+        }
+    }
     return (<>
         <Header />
         <div className={"container"}>
-            <FeedbackList feedback={feedback}/>
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
     </>)
 }
