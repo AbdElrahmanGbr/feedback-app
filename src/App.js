ad97119aa0699efa0605df9e770cc0eb032f8a89
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-12-26 11:40:31 +0200

    Feedback Stats Component and Reactivity

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import Header from "./components/Header";
 import {useState} from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
-    const deleteFeedback = (id) =>{
-        if(window.confirm('Are you Sure you want to DELETE?')){
-            setFeedback(feedback.filter((item)=> item.id !== id))
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you Sure you want to DELETE?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
     return (<>
-        <Header />
+        <Header/>
         <div className={"container"}>
+            <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
     </>)
